1b95a6d25aac96bfa48352ffcfe876b135b3ff7e
Jonathan Dahan <jonathan@jonathan.is>
2015-10-13 14:26:01 -0400

    Allow for no string to be returned in find_best_string

--- challenge4.py
+++ challenge4.py
@@ -3,8 +3,8 @@ from challenge3 import find_best_string, score
 
 if __name__ == "__main__":
   lines = [line.strip() for line in open('input/4.txt').readlines()]
-  strings = [find_best_string(line) for line in lines]
+  strings = [find_best_string(line) or '' for line in lines]
   scores = [score(string) for string in strings]
-  best = strings[scores.index(max(scores))]
+  best = strings[scores.index(max(scores))].strip()
   assert(best == "Now that the party is jumping")
   print(best)
