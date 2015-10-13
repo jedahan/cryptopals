7e08dbddf9556c66dec148f6ff22f59b6508d879
Jonathan Dahan <jonathan@jonathan.is>
2015-10-13 11:43:13 -0400

    DRY up xor a bit

--- challenge2.py
+++ challenge2.py
@@ -1,13 +1,13 @@
 import base64
 from challenge1 import hex264
 
+# Expects cryp and key to be a hex string, returns a byte string/byte array
 def xor(cryp, key):
+  if len(key) == 1: key = len(crypt) * key
   crypt = hex264(cryp)
   key = hex264(key)
-  if(len(key)==1):
-    return bytes([ crypt[i] ^ key for i in range(len(crypt)) ])
-  else:
-    return bytes([ crypt[i] ^ key[i] for i in range(len(crypt)) ])
+
+  return bytes([ crypt[i] ^ key[i] for i in range(len(crypt)) ])
 
 if __name__ == "__main__":
   decoded = xor('1c0111001f010100061a024b53535009181c', '686974207468652062756c6c277320657965').decode('utf-8')
