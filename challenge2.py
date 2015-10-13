4ebf086a3fe2e790b838d27ef7bb4629d9b57659
Jonathan Dahan <jonathan@jonathan.is>
2015-10-13 12:10:09 -0400

    Make the key length the same as the encrypted message length

--- challenge2.py
+++ challenge2.py
@@ -2,12 +2,15 @@ import base64
 from challenge1 import hex264
 
 # Expects cryp and key to be a hex string, returns a byte string/byte array
-def xor(cryp, key):
-  if len(key) == 1: key = len(crypt) * key
-  crypt = hex264(cryp)
+def xor(encrypted, key):
+  if len(key) != len(encrypted):
+    if len(key) == 1: key = ("0" + key)
+    key = int(len(encrypted)/len(key)) * key
+
+  encrypted = hex264(encrypted)
   key = hex264(key)
 
-  return bytes([ crypt[i] ^ key[i] for i in range(len(crypt)) ])
+  return bytes([ encrypted[i] ^ key[i] for i in range(len(encrypted)) ])
 
 if __name__ == "__main__":
   decoded = xor('1c0111001f010100061a024b53535009181c', '686974207468652062756c6c277320657965').decode('utf-8')
