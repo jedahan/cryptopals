47128f2492a26a0583b7b11a62f116234c27b2ca
Jonathan Dahan <jonathan@jonathan.is>
2015-10-15 12:49:00 -0400

    Return the key with the decrypted string as a tuple

--- challenge3.py
+++ challenge3.py
@@ -59,9 +59,9 @@ def find_best_string(encrypted):
   encrypted = [x for x in base64.b16decode(encrypted, True)]
   key = guess_single_key(encrypted)
   decrypted = decrypt(encrypted, key)
-  return decrypted
+  return (key, decrypted)
 
 if __name__ == "__main__":
-  decrypted = find_best_string("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+  key, decrypted = find_best_string("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
   assert(decrypted == "Cooking MC's like a pound of bacon")
-  print(decrypted)
+  print(key, decrypted)
--- challenge4.py
+++ challenge4.py
@@ -4,7 +4,8 @@ from challenge3 import find_best_string, score
 if __name__ == "__main__":
   lines = [line.strip() for line in open('input/4.txt').readlines()]
   strings = [find_best_string(line) or '' for line in lines]
-  scores = [score([x for x in bytes(string,'utf8')]) for string in strings]
-  best = strings[scores.index(max(scores))].strip()
-  assert(best == "Now that the party is jumping")
-  print(best)
+  scores = [score([x for x in bytes(string,'utf8')]) for key, string in strings]
+  key, decrypted = strings[scores.index(max(scores))]
+  decrypted = decrypted.strip()
+  assert(decrypted == "Now that the party is jumping")
+  print(" xor %r => %r" % (key, decrypted))
